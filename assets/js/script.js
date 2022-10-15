8538f8fb611b462b7b7cb145f75d53b862ae6cc1
Abhay Verma <abhayverma2505@gmail.com>
2022-10-15 15:31:00 +0530

    script

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,11 +33,11 @@ function expCalculator() {
 $(document).ready(function() {
 
    
-    if ((screen.width<=460))  {
+    if ((screen.width<=500))  {
     // if screen size width is less than 1024px
     document.getElementById("hide").style.display = "none";
     }
-    if ((screen.width>460))  {
+    if ((screen.width>500))  {
         // if screen size width is less than 1024px
         document.getElementById("hide").style.display = "block";
         }
